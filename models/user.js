f1ebfa81d932da3e847b73d0602eb6c8887f194b
MoatazMhamdi <Moataz.mhamdi@esprit.tn>
2023-11-19 02:02:27 +0100

    http request
    here i've fixed in my code the urlsession on the services through file called apiManger

--- models/user.js
+++ models/user.js
@@ -1,13 +1,27 @@
 import mongoose from 'mongoose'; // Importer Mongoose
 const { Schema, model } = mongoose; // Utiliser Schema et model du module mongoose
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: false },
-    password: { type: String, required: true },
-    numTel: { type: Number, required: true ,unique:true },
+    name: { 
+      type: String, 
+      required: true },
+
+    email: { 
+      type: String, 
+      required: false },
+    password: { 
+      type: String, 
+      required: true },
+    numTel: { 
+      type: Number, 
+      required: true ,
+      unique:true },
    
-    picture: { type: String, required : false },
-    role: { type: String, enum: ['Farmer', 'Client', 'AdminSup'], required: true },   
+    picture: { type: String, 
+      required : false },
+    role: { type: String, 
+      enum: ['Farmer', 'Client', 'AdminSup'], 
+      
+      required: true },   
   });
 
   const User = mongoose.model('User', userSchema);
